7a52d46715a25243ed4f9cc0b9659c9e8b1ff906
GrE3d0 <43867387+GrE3d0@users.noreply.github.com>
2020-04-05 23:50:37 -0400

    Update BotControl.py
    BotControl will now check turns in order to make a decision with all bots other than Random Algorithm.  It will choose the middle column if the turn isn't the third; when it reaches that point, the Bot chosen will then execute its functions in order to make a move.

--- BotControl.py
+++ BotControl.py
@@ -1,28 +1,36 @@
 '''
 Rajinder & Pete
-Version 0
-2/19/2020
+Version 0.02
+4/5/2020
 Main Function
     Handles all the interactions between Game Function & Algorithms
     Selects the algorithm
 '''
 import algR
+import algDef
 from Board import Board
 
 selCol = -1 # Selected Column by the algorithm ( Default value of -1 for error checking)
 algnum = 1 # Chooses algorithm (Make sure to only change when game is won or loss)
 
 #Constructor
-def __init__(key, board):
-    selCol = selectAlgorithm(key, board)
-    return selCol
+def __init__(key, board, turn):
+    if key is 1:
+        selCol = selectAlgorithm(key, board)
+        return selCol
+    elif key is not 1:
+        if turn < 3:
+            return 3
+        else:
+            selCol = selectAlgorithm(key, board)
+            return selCol
 
 #Selects the appropriate algorithm
 def selectAlgorithm(key, board):
     if key == 1:
         return random(board)
     elif key == 2:
-        return "Haha yup"
+        return defensive(board)
     elif key == 3:
         return "No chance"
     elif key == 4:
@@ -34,9 +42,10 @@ def selectAlgorithm(key, board):
 #Defines each algorithm and associates to the corresponding file
 def random(board):
         return algR.__init__(board)
-def defensive():
-        return "file"
+def defensive(board):
+        return algDef.__init__(board)
 def offensive():
         return "file"
 def minmax():
         return "file"
+
