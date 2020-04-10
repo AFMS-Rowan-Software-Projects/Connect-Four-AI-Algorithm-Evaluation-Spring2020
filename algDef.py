5cf04d7573e6f86647eb3f2bdc07ec0bb0249cd1
Not-Joshie <60893763+Not-Joshie@users.noreply.github.com>
2020-04-10 11:23:29 -0400

    Update algDef.py
    - The bot now avoids making moves that will give the player a win condition, if possible.

--- algDef.py
+++ algDef.py
@@ -1,6 +1,6 @@
 # Rajinder , Pete, Josh, Ian
-#   Version 0.03
-#   4/9/2020
+#   Version 0.04
+#   4/10/2020
 #
 # Defensive algorithm
 #   Will try to force draws on the board
@@ -19,7 +19,9 @@ def __init__(board):
     fix_board(tempBoard)
     find_edges(tempBoard)
     calculate_edges(tempBoard)
-    return pick_col()
+    print("board")
+    tempBoard.printBoard()
+    return pick_col(tempBoard)
 
     #functions to place
 
@@ -31,7 +33,7 @@ def fix_board(board):
                 board.set_board(currRow, currCol, True)
             elif (board.search(currRow, currCol) == 2 or board.search(currRow, currCol) is False):  # Checks for bot piece
                 board.set_board(currRow, currCol, False)
-    board.printBoard()
+    #board.printBoard()
 
  # Finds outer edge pieces on the board set to True e.g. human placed pieces with free slots around them set in
  #dictionary which has initial value of 0 for each key/coordinate tuple
@@ -45,29 +47,24 @@ def find_edges(board):
             #Check to see if current position is set to True clockwise i.e. Player's piece
             if board.search(currRow, currCol) is True:
                 #Checks which see if
-                if board.inbound(currRow - 1, currCol - 1) and board.search(currRow - 1, currCol - 1) is 0 \
-                        and not board.search(currRow - 2, currCol -1) is 0:
+                if board.inbound(currRow - 1, currCol - 1) and board.search(currRow - 1, currCol - 1) is 0:
                     edges.append((currRow - 1, currCol - 1))
-                if board.inbound(currRow, currCol - 1) and board.search(currRow, currCol - 1) is 0 \
-                        and not board.search(currRow - 1, currCol -1) is 0:
+                if board.inbound(currRow, currCol - 1) and board.search(currRow, currCol - 1) is 0:
                     edges.append((currRow, currCol - 1))
-                if board.inbound(currRow + 1, currCol - 1) and board.search(currRow + 1, currCol - 1) is 0 \
-                        and not board.search(currRow, currCol - 1) is 0:
+                if board.inbound(currRow + 1, currCol - 1) and board.search(currRow + 1, currCol - 1) is 0:
                     edges.append((currRow + 1, currCol - 1))
-                if board.inbound(currRow + 1, currCol) and board.search(currRow + 1, currCol) is 0 \
-                        and not board.search(currRow, currCol) is 0:
+                if board.inbound(currRow + 1, currCol) and board.search(currRow + 1, currCol) is 0:
                     edges.append((currRow + 1, currCol))
-                if board.inbound(currRow + 1, currCol + 1) and board.search(currRow + 1, currCol + 1) is 0 \
-                        and not board.search(currRow, currCol + 1) is 0:
+                if board.inbound(currRow + 1, currCol + 1) and board.search(currRow + 1, currCol + 1) is 0:
                     edges.append((currRow + 1, currCol + 1))
-                if board.inbound(currRow, currCol + 1) and board.search(currRow, currCol + 1) is 0 \
-                        and not board.search(currRow - 1, currCol + 1) is 0:
+                if board.inbound(currRow, currCol + 1) and board.search(currRow, currCol + 1) is 0:
                     edges.append((currRow, currCol + 1))
-                if board.inbound(currRow -1, currCol + 1) and board.search(currRow - 1, currCol + 1) is 0 \
-                        and not board.search(currRow - 2, currCol + 1) is 0:
+                if board.inbound(currRow -1, currCol + 1) and board.search(currRow - 1, currCol + 1) is 0:
                     edges.append((currRow -1, currCol + 1))
     #Removes duplicates from the array before inserting into a dictionary for all keys to be different
     edges = list(dict.fromkeys(edges))
+    #print(edges)
+    #print("edges")
     #Inserts coordinate tuples into the dictionary, setting all values to 0.
     for value in edges:
         if board.search(value[0], value[1]) is 0:
@@ -153,19 +150,41 @@ def calculate_edges(board):
             if piece == 1:
                 temp = 1
             elif piece == 2:
-                temp = 7
+                temp = 3
             elif piece == 3:
                 temp = 20
             summation += temp
+
         coordinates[position] = summation
+    # Loops through each column and adds to the set of coordinates is the row is empty and not an edge
+    for col in range(0, 7):
+        if board.search(0, col) is 0:
+
+            if board.search(0, col - 1) is 0 or not board.inboundCol(col - 1):
+                if board.search(0, col + 1) is 0 or not board.inboundCol(col + 1):
+                    coordinates[(0, col)] = 0
+    # Loop through the coordinates and check if the position above could be a win condition
+    # If it is, set its value to a negative to prevent the bot from making that move
+    for point in coordinates:
+        if not coordinates.get((point[0] + 1, point[1]), -1) is -1:
+            if coordinates[point[0] + 1, point[1]] > 20:
+                coordinates[point] = -50
+
+    # Checks if a coordinate is 'floating' and if it is, set its value to a negative to prevent
+    # the bot from making that move
+    for point in coordinates:
+        if board.search(point[0] - 1, point[1]) is 0:
+            coordinates[point] = -10
+
 
 # Returns the column from dictionary associated with the
 # position with the highest score made by the bot for User Interface
 # @return Integer column to place on board against player
-def pick_col():
+def pick_col(board):
     maximum = 0 # Integer which is set to the maximum value found in the dictionary
     column = 0  # Integer of the y-position of the position which has
                 # the greatest score based on player's piece positions
+
     for key in coordinates:
         if maximum is 0:
             maximum = coordinates[key]
