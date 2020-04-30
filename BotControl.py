92941debf0fb7076251df8aa055a5b433ca2e021
GrE3d0 <43867387+GrE3d0@users.noreply.github.com>
2020-04-30 17:22:00 -0400

    Square Move Implementation
    Passes through Square Move Mode into Bot.py through BotControl.py

--- BotControl.py
+++ BotControl.py
@@ -1,7 +1,7 @@
 '''
 Rajinder, Pete, Josh, Ian
-Version 0.6
-4/29/2020
+Version 0.7
+4/30/2020
 Main Function
     Handles all the interactions between Game Function & Algorithms
     Selects the algorithm
@@ -14,7 +14,7 @@ selCol = -1 # Selected Column by the algorithm ( Default value of -1 for error c
 algnum = 1 # Chooses algorithm (Make sure to only change when game is won or loss)
 
 #Constructor
-def __init__(key, board, turn):
+def __init__(key, board, turn, mode):
     if key is 1:
         selCol = selectAlgorithm(key, board)
         return selCol
@@ -22,29 +22,29 @@ def __init__(key, board, turn):
         if turn < 1:
             return 3
         else:
-            selCol = selectAlgorithm(key, board)
+            selCol = selectAlgorithm(key, board, mode)
             return selCol
 
 #Selects the appropriate algorithm
-def selectAlgorithm(key, board):
+def selectAlgorithm(key, board, mode):
     if key == 1:
         return random(board)
     elif key == 2:
-        return defensive(board)
+        return defensive(board, mode)
     elif key == 3:
-        return offensive(board)
+        return offensive(board, mode)
     elif key == 4:
-        return minmax(board)
+        return minmax(board, mode)
 
 
 
 
 #Defines each algorithm and associates to the corresponding file
 def random(board):
-        return Bot.__init__(board, 0, False)
-def defensive(board):
-        return Bot.__init__(board, 1, False)
-def offensive(board):
-        return Bot.__init__(board, 2, False)
-def minmax(board):
-        return MinMax.__init__(board)
+        return Bot.__init__(board, 0, False, False)
+def defensive(board, mode):
+        return Bot.__init__(board, 1, False, mode)
+def offensive(board, mode):
+        return Bot.__init__(board, 2, False, mode)
+def minmax(board, mode):
+        return MinMax.__init__(board, mode)
